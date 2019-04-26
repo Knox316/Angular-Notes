9d54342efdf7ba1f199bb38ab360330e3135b65d
Knox316 <fernandocorreia316@gmail.com>
2019-04-26 14:50:07 +0100

    add print func

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -50,6 +50,26 @@ export class ReadComponent implements OnInit {
     doc.save("notes.pdf");
   }
 
+  //incomplete
+  //printing the entire page and not only the notes
+  print(): void {
+    let printContents, popupWin;
+    printContents = document.getElementById("print").innerHTML;
+    popupWin = window.open("", "_blank", "top=0,left=0,height=100%,width=auto");
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>Cocus Challenge</title>
+          <style>
+          //........Customized style.......
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`);
+    popupWin.document.close();
+  }
+
   updateNote(index) {}
   ngOnInit() {}
 }
